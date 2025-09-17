26de9d583e9d1fd9d11932c3bd1061c222827ab2
jazminruizdiaz <jazmin.ruiz@assuresoft.tech>
2025-09-17 17:21:44 -0300

    remove all button added

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const input = document.getElementById("input");
 const addButton = document.getElementById("add");
 const list = document.getElementById("list");
+const removeButton = document.getElementById("remove");
 
 addButton.addEventListener("click", () => {
-    const task = input.value;
-    if (task === "") {
-        return;
-    }
-    const li = document.createElement("li");
-    li.textContent = task;
-    list.appendChild(li);
+  const task = input.value;
+  if (task === "") {
+    return;
+  }
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  list.appendChild(li);
+  input.value = "";
+});
+
+removeButton.addEventListener("click", () => {
+  list.innerHTML = "";
 });
